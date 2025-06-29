ebd111e119ed58fac0027c232c94c18ce9c52dbe
rumahkodingku <rumahkodingku45@gmail.com>
2025-06-29 10:37:36 +0700

    refactor: change posititon menu navbar

--- src/constants/nav-links.ts
+++ src/constants/nav-links.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
     { name: 'Home', href: '/' },
+    { name: 'About', href: '/about' },
     { name: 'Features', href: '/features' },
     { name: 'How it Works', href: '/how-it-works' },
-    { name: 'About', href: '/about' },
 ];
