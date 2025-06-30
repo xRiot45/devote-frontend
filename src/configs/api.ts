21f6532adfe62bb3169eada7b1df6a242d2b650e
rumahkodingku <rumahkodingku45@gmail.com>
2025-06-30 16:17:57 +0700

    feat: create api config

---
+++ src/configs/api.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import router from 'next/router';
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api',
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = Cookies.get('accessToken');
+
+        if (token && config.headers) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
+);
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error?.response) {
+            const status = error.response.status;
+
+            if (status == 401) {
+                Cookies.remove('accessToken');
+                router.push('/');
+            }
+        }
+
+        return Promise.reject(error);
+    },
+);
