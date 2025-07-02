1364c692a259aca299f25335716a81da31f9d2e9
rumahkodingku <rumahkodingku45@gmail.com>
2025-07-02 16:37:03 +0700

    refactor: change ssr from true to false

--- src/providers/web3-provider.tsx
+++ src/providers/web3-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { wagmiConfig } from '@/lib/wagmi';
+import { wagmiConfig } from '@/configs/wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
