2d4c18f3d27ad67b207accc81a49aaf4aa96d031
rumahkodingku <rumahkodingku45@gmail.com>
2025-07-02 17:00:16 +0700

    feat: create page step for auth

---
+++ src/app/auth/step-1.tsx
@@ -0,0 +1,7 @@
+export default function Step1Page() {
+    return (
+        <>
+            <h1>Step 1</h1>
+        </>
+    );
+}
---
+++ src/app/auth/step-2.tsx
@@ -0,0 +1,7 @@
+export default function Step2Page() {
+    return (
+        <>
+            <h1>Step 2</h1>
+        </>
+    );
+}
